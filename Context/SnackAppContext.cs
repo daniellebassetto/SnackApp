a19eaef67bd78088d33a0530cc1e608334478b29
Danielle Bassetto <danibbassetto@hotmail.com>
2024-10-10 22:34:46 -0300

    Adicionar arquivos de projeto.

---
+++ Context/SnackAppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SnackApp.Models;
+
+namespace SnackApp.Context;
+
+public class SnackAppContext : DbContext
+{
+    public SnackAppContext(DbContextOptions<SnackAppContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Lanche> Lanches { get; set; }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using SnackApp.Models;
+using System.Diagnostics;
+
+namespace SnackApp.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
