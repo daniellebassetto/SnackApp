bac4abf79c33adcc53dba2b237ab68618f0c3646
Danielle Bassetto <danibbassetto@hotmail.com>
2024-10-11 22:11:11 -0300

    feat(listagem de lanches)

---
+++ Controllers/LancheController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SnackApp.Repositories.Interfaces;
+using SnackApp.ViewModels;
+
+namespace SnackApp.Controllers;
+
+public class LancheController(ILancheRepository lancheRepository) : Controller
+{
+    private readonly ILancheRepository _lancheRepository = lancheRepository;
+
+    public IActionResult List()
+    {
+        var lancheListViewModel = new LancheListViewModel();
+        var lanches = _lancheRepository.Lanches;
+        lancheListViewModel.Lanches = lanches;
+        lancheListViewModel.CategoriaAtual = "Categoria atual";
+        
+        return View(lanches);
+    }
+}
